1707fbada688bcd3a407c6a417cecff695faa6f6
HaruAlex <yn84258@gmail.com>
2025-05-07 14:43:52 +0700

    kệ tụi bay

---
+++ Database/db_connect.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";      
+$username = "root";      
+$password = "";          
+$database = "travel_blog"; 
+
+// Kết nối
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Kiểm tra kết nối
+if (!$conn) {
+    die("Kết nối thất bại: " . mysqli_connect_error());
+} 
+?>
